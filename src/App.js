cec38f18c5a03a4e02bfae535ce52218900c93d6
Meguna Okawa <megunaokawa@gmail.com>
2022-01-11 20:59:31 +0900

    init repo

---
+++ src/App.js
@@ -0,0 +1,66 @@
+/// app.js
+import React from 'react';
+import DeckGL from '@deck.gl/react';
+import { GeoJsonLayer } from '@deck.gl/layers';
+import { StaticMap } from 'react-map-gl';
+import "App.css";
+// import { map } from 'kepler.gl.html';
+
+import  polling_data from 'data/polling_locations.json';
+
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoibWVnbzk5IiwiYSI6ImNrMXc1czBuajA4OGIzbWxyeTB0M3ZjYnIifQ.XPSU11b9t3LASlHTE-4ptg";
+const INITIAL_VIEW_STATE = {
+  longitude: -71.0589,
+  latitude: 42.364506,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+
+function App() {
+  const layers = [
+    new GeoJsonLayer({
+      id: 'scatterplot-layer',
+      data: polling_data,
+      pickable: true,
+      stroked: true,
+      filled: true,
+      extruded: true,
+      pointType: 'circle',
+      getPointRadius: 50,
+      lineWidthScale: 20,
+      lineWidthMinPixels: 2,
+      getFillColor: [160, 160, 180, 200],
+      getStrokeColor: [160, 160, 180, 200],
+      getStrokeWidth: 50,
+      getLineWidth: 1,
+      getElevation: 30
+    })
+  ];
+  console.log(polling_data);
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        My Interactive Data Visualization
+      </header>
+      <div className="map-wrapper">
+        {/* <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller={true}
+          layers={layers}>
+          <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+        </DeckGL> */}
+        <iframe src={`${process.env.PUBLIC_URL}/kepler.gl.html`} />
+      </div>
+      <p className="app-description">
+        This is my interactive data visualization. It uses a really interesting data set called DATASET.
+        Using this visualization, you can interact by CLICKING, DRAGGING, and more.
+        Please explore this dataset!
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Notes.md
@@ -0,0 +1,10 @@
+Common Pitfalls
+
+* Rename geojson files to .json (quirk for CRA, it's only been configured to handle jsons by default)
+
+* if the data doesn't show up, make sure to check that 
+1) it's formatted correctly. there can be weird null values in raw data downloaded from govt sites
+2) try to increase the visibility of the visuals of the layer (set a huge stroke, a fill in a bright color, etc). 
+   be outrageous. sometimes it's just a matter of having the right zoom level.
+
+* use mapshaper to visualize data
